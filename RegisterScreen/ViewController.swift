ddd2634b13131b6ee4d86b6edb4db1da86d43c88
MrLioner <testing07524@gmail.com>
2022-02-16 21:36:30 -0500

    Initial Commit

---
+++ RegisterScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegisterScreen
+//
+//  Created by Pablo De La Cruz on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
